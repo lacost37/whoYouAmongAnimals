6add4a4f544f8979c300a0ab198787f464c21062
Ivan <saitez377@gmail.com>
2022-09-03 18:48:36 +0300

    Added description and first code

---
+++ whoYouAmongAnimals/View Controllers/FinalViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FinalViewController.swift
+//  whoYouAmongAnimals
+//
+//  Created by Mac on 03.09.2022.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+}
---
+++ whoYouAmongAnimals/View Controllers/QuestionViewContoller.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewContoller.swift
+//  whoYouAmongAnimals
+//
+//  Created by Mac on 03.09.2022.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
